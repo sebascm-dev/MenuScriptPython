80c001264bd25509039d755a8d66eed4c316ffaa
sebascm14 <43911461+sebascm14@users.noreply.github.com>
2023-05-05 01:56:14 +0200

    Update script1.py

--- script1.py
+++ script1.py
@@ -3,7 +3,6 @@ import sys
 import os
 import time
 import subprocess
-import psutil
 import winreg
 import requests
 import zipfile
@@ -45,8 +44,7 @@ while True:
     print()
     print(Fore.GREEN + '1. Activaciones Windows' + Style.RESET_ALL)
     print(Fore.GREEN + '2. Descargar Office-2021' + Style.RESET_ALL)
-    print(Fore.GREEN + '3. Mostrar almacenamiento' + Style.RESET_ALL)
-    print(Fore.RED + '4. Salir' + Style.RESET_ALL)
+    print(Fore.RED + '99. Salir' + Style.RESET_ALL)
     print()
 
     opcion = input('Seleccione una opcion: ')
@@ -62,7 +60,7 @@ while True:
             print()
             print(Fore.GREEN + '1. Activar Windows PRO' + Style.RESET_ALL)
             print(Fore.GREEN + '2. Activar Windows HOME' + Style.RESET_ALL)
-            print(Fore.RED + '3. Volver' + Style.RESET_ALL)
+            print(Fore.RED + '99. Volver' + Style.RESET_ALL)
             print()
 
             opcionActivacion = input('Seleccione una opcion: ')
@@ -71,18 +69,21 @@ while True:
 
                 os.system('cls' if os.name == 'nt' else 'clear')
 
-                print(Fore.YELLOW + '[+] ACTIVANDO WINDOWS PRO...' + Style.RESET_ALL)
+                print(Fore.YELLOW + '╔═══════════════════════════╗' + Style.RESET_ALL)
+                print(Fore.YELLOW + '║ ACTIVACION DE WINDOWS PRO ║' + Style.RESET_ALL)
+                print(Fore.YELLOW + '╚═══════════════════════════╝' + Style.RESET_ALL)
                 print()
+                time.sleep(2)
 
-                print('[+] EJECUTANDO EL COMANDO: slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX')
+                print(Fore.YELLOW + '[/]' + Style.RESET_ALL + ' EJECUTANDO EL COMANDO: slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX')
                 subprocess.run(['slmgr', '/ipk', 'W269N-WFGWX-YVC9B-4J6C9-T83GX'], shell=True)
-                print('[+] EJECUTANDO EL COMANDO: slmgr /skms kms.digiboy.ir')
+                print(Fore.YELLOW + '[/]' + Style.RESET_ALL + ' EJECUTANDO EL COMANDO: slmgr /skms kms.digiboy.ir')
                 subprocess.run(['slmgr', '/skms', 'kms.digiboy.ir'], shell=True)
-                print('[+] EJECUTANDO EL COMANDO: slmgr /ato')
+                print(Fore.YELLOW + '[/]' + Style.RESET_ALL + ' EJECUTANDO EL COMANDO: slmgr /ato')
                 subprocess.run(['slmgr', '/ato'], shell=True)
                 print()
 
-                print(Fore.YELLOW + '[+] LA ACTIVACION HA FINALIZADO CORRECTAMENTE...' + Style.RESET_ALL)
+                print(Fore.GREEN + '[+]' + Style.RESET_ALL + ' LA ACTIVACION HA FINALIZADO CORRECTAMENTE')
                 print()
                 time.sleep(2)
                 
@@ -93,24 +94,27 @@ while True:
 
                 os.system('cls' if os.name == 'nt' else 'clear')
 
-                print('[+] ACTIVANDO WINDOWS PRO...')
+                print(Fore.YELLOW + '╔════════════════════════════╗' + Style.RESET_ALL)
+                print(Fore.YELLOW + '║ ACTIVACION DE WINDOWS HOME ║' + Style.RESET_ALL)
+                print(Fore.YELLOW + '╚════════════════════════════╝' + Style.RESET_ALL)
                 print()
+                time.sleep(2)
 
-                print('[+] EJECUTANDO EL COMANDO: slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99')
+                print(Fore.YELLOW + '[/]' + Style.RESET_ALL + ' EJECUTANDO EL COMANDO: slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99')
                 subprocess.run(['slmgr', '/ipk', 'TX9XD-98N7V-6WMQ6-BX7FG-H8Q99'], shell=True)
-                print('[+] EJECUTANDO EL COMANDO: slmgr /skms kms.digiboy.ir')
+                print(Fore.YELLOW + '[/]' + Style.RESET_ALL + ' EJECUTANDO EL COMANDO: slmgr /skms kms.digiboy.ir')
                 subprocess.run(['slmgr', '/skms', 'kms.digiboy.ir'], shell=True)
-                print('[+] EJECUTANDO EL COMANDO: slmgr /ato')
+                print(Fore.YELLOW + '[/]' + Style.RESET_ALL + ' EJECUTANDO EL COMANDO: slmgr /ato')
                 subprocess.run(['slmgr', '/ato'], shell=True)
                 print()
 
-                print('[+] LA ACTIVACION HA FINALIZADO CORRECTAMENTE...')
+                print(Fore.GREEN + '[+]' + Style.RESET_ALL + ' LA ACTIVACION HA FINALIZADO CORRECTAMENTE')
                 print()
                 time.sleep(2)
                 
                 os.system('cls' if os.name == 'nt' else 'clear')
 
-            elif opcionActivacion == '3':
+            elif opcionActivacion == '99':
                 os.system('cls' if os.name == 'nt' else 'clear')
                 break
 
@@ -128,7 +132,7 @@ while True:
         print()
 
         # Desactivar la monitorización en tiempo real
-        print(Fore.RED + '[-]' + Style.RESET_ALL + ' DESACTIVANDO LA MONOTORIZACION EN TIEMPO REAL')
+        print(Fore.RED + '[-]' + Style.RESET_ALL + ' DESACTIVANDO LA PROTECCION EN TIEMPO REAL')
         comando = "powershell -command \"Set-MpPreference -DisableRealtimeMonitoring $true\""
         subprocess.run(comando, shell=True)
         # Desactivar la protección basada en la nube
@@ -147,7 +151,7 @@ while True:
 
 
         # Descargar
-        print(Fore.YELLOW + '[+]' + Style.RESET_ALL + 'DESCARGANDO OFFICE-2021.ZIP')
+        print(Fore.YELLOW + '[/]' + Style.RESET_ALL + ' DESCARGANDO OFFICE-2021.ZIP')
         url = 'https://github.com/sebascm14/sebascm14/raw/main/Office%202021.zip'
         archivo_destino = 'Office 2021.zip'
 
@@ -157,65 +161,29 @@ while True:
             f.write(response.content)
         
         # Descomprimir
-        print(Fore.YELLOW + '[+]' + Style.RESET_ALL + 'DESCOMPRIMIENDO ARCHIVO...')
+        print(Fore.YELLOW + '[/]' + Style.RESET_ALL + ' DESCOMPRIMIENDO ARCHIVO...')
         with zipfile.ZipFile(archivo_destino, 'r') as zip_ref:
             zip_ref.extractall('.')
 
         # Instalar
-        print(Fore.YELLOW + '[+]' + Style.RESET_ALL + 'ARCHIVO DESCOMPRIMIDO...')
+        print(Fore.YELLOW + '[/]' + Style.RESET_ALL + ' ARCHIVO DESCOMPRIMIDO...')
         time.sleep(2)
-        print(Fore.YELLOW + '[+]' + Style.RESET_ALL + 'EJECUTANDO OFFICE 2021')
-        print(Fore.GREEN + '[+]' + Style.RESET_ALL + 'INSTALANDO...')
+
+        # Ejecutar
+        print(Fore.YELLOW + '[/]' + Style.RESET_ALL + ' EJECUTANDO OFFICE 2021')
+        print(Fore.GREEN + '[+]' + Style.RESET_ALL + ' INSTALANDO...')
         os.system('cmd /c "cd OFFICE 2021 && setup.exe /configure configuration.xml"')
 
-        print(Fore.GREEN + '[+]' + Style.RESET_ALL + 'EL SOFTWARE DE OFFICE HA SIDO INSTALADO CORRECTAMENTE')
+        print()
+        print(Fore.GREEN + '[+]' + Style.RESET_ALL + ' EL SOFTWARE DE OFFICE HA SIDO INSTALADO CORRECTAMENTE')
         time.sleep(3)
 
         os.system('cls' if os.name == 'nt' else 'clear')
 
 
-    
-    elif opcion == '3':
-        while True:
-            print('Mostrando almacenamiento...')
-            os.system('cls' if os.name == 'nt' else 'clear')
-
-            partitions = psutil.disk_partitions(all=True)
-
-            for idx, partition in enumerate(partitions):
-                print(f'{idx}. Disco: {partition.device}')
-                print(f'Punto de montaje: {partition.mountpoint}')
-                print(f'Formato: {partition.fstype}')
-
-                usage = psutil.disk_usage(partition.mountpoint)
-                total = usage.total
-                used = usage.used
-                free = usage.free
-
-                bar_length = 20
-                used_percent = int(used / total * 100)
-                bar_used = int(used_percent / 100 * bar_length)
-                bar_free = bar_length - bar_used
-
-                print(f'Almacenamiento usado: {used_percent}% [{"=" * bar_used}{" " * bar_free}]')
-                print(f'Tamaño total: {total / (1024 * 1024 * 1024):.2f} GB')
-                print(f'Espacio libre: {free / (1024 * 1024 * 1024):.2f} GB\n')
-
-            print('\033[33mOpciones:\033[m')
-            print('\033[31m1. Volver al menú principal\033[m')
-
-            seleccion = input("Seleccione una opción: ")
-
-            if seleccion == "1":
-                os.system('cls' if os.name == 'nt' else 'clear')
-                break
-                
-            else:
-                print("Opción inválida. Intente nuevamente.")
-                input("Presione cualquier tecla para continuar...")
-        
-    elif opcion == '4':
+    elif opcion == '99':
         print('Saliendo...')
         sys.exit()
+    
     else:
         print('Opción inválida')
